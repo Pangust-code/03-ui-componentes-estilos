c6b49b200909897d7045edfadfe58d3a0e0596fd
Pangust-code <www.daniku134@gmail.com>
2025-11-21 18:00:22 -0500

    frist commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { Component } from '@angular/core';
+import { EstilosPage } from './features/EstilosPage/EstilosPage';
+
+export const routes: Routes = [
+
+  {
+    path: '',
+    component: EstilosPage
+  },
+
+];
